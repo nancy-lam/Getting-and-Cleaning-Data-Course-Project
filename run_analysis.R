6292629d4c185ccf1d8a6c192a4d38a220bfefee
Hoang Lam <33915376+ulamht@users.noreply.github.com>
2018-01-17 19:28:07 -0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,43 @@
+OVERVIEW
+#   Using data collected from the accelerometers from the Samsung Galaxy S 
+#   smartphone, work with the data and make a clean data set, outputting the
+#   resulting tidy data to a file named "tidy_data.txt".
+#   See README.md for details
 
+library(dplyr)
+
+#STEP 1: GET DATA
+
+# download zip file containing data if it hasn't already been downloaded
+zipUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+zipFile <- "UCI HAR Dataset.zip
+if (!file.exists(zipFile)) {
+  download.file(zipUrl, zipFile, mode = "wb")
+}
+
+# unzip zip file containing data if data directory doesn't already exist
+dataPath <- "UCI HAR Dataset"
+if (!file.exists(dataPath)) {
+  unzip(zipFile)
+}
+
+#STEP 2: READ DATA
+
+# read training data
+trainingSubjects <- read.table(file.path(dataPath, "train", "subject_train.txt"))
+trainingValues <- read.table(file.path(dataPath, "train", "X_train.txt"))
+trainingActivity <- read.table(file.path(dataPath, "train", "y_train.txt"))
+
+# read test data
+testSubjects <- read.table(file.path(dataPath, "test", "subject_test.txt"))
+testValues <- read.table(file.path(dataPath, "test", "X_test.txt"))
+testActivity <- read.table(file.path(dataPath, "test", "y_test.txt"))
+
+# read features, don't convert text labels to factors
+features <- read.table(file.path(dataPath, "features.txt"), as.is = TRUE)
+  ## note: feature names (in features[, 2]) are not unique
+  ##       e.g. fBodyAcc-bandsEnergy()-1,8
+
+# read activity labels
+activities <- read.table(file.path(dataPath, "activity_labels.txt"))
+colnames(activities) <- c("activityId", "activityLabel")
